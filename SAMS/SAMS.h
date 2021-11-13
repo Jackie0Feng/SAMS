151ed49afa3d04e842fbf4abc868d043be2f35e1
jackie feng <2571617419@qq.com>
2021-11-13 12:30:26 +0800

    V5.0更新，更改学生数据结构

--- SAMS/SAMS.h
+++ SAMS/SAMS.h
@@ -1,9 +1,10 @@
 /*程序功能:   学生成绩管理
 编码者:   JackieFeng
-日期:  08/11/2021
-版本号:4.0
+日期:  13/11/2021
+版本号:5.0
 备注：
-某班有最多不超过30人(具体人数由键盘输入）参加期末考试，考试科目最多不超过6门（具体门数由键盘输入）。用二维数组做函数参数编程实现如下菜单驱动的学生成绩管理系统：
+某班有最多不超过30人(具体人数由键盘输入）参加期末考试，考试科目最多不超过6门（具体门数由键盘输入）。
+定义结构体类型，用结构体数组作函数参数编程实现如下菜单驱动的学生成绩管理系统：
 (1)录入每个学生的学号，姓名和各科考试成绩；
 (2)计算每门课程的总分和平均分；
 (3)计算每个学生的总分和平均分
@@ -19,6 +20,22 @@
 #define STU_NUM 30      //最多人数
 #define MAX_LEN 10      //名字最大长度
 #define COURSE_NUM 6//课程数上限
+
+//学生结构体
+//long num;
+//char name[MAX_LEN];
+//float score[COURSE_NUM];
+//float sum;
+//float aver;
+typedef struct student
+{
+    long num;
+    char name[MAX_LEN];
+    float score[COURSE_NUM];
+    float sum;
+    float aver;
+}STU;
+
 /*
 函数名称:   Mean
 功能描述:   显示菜单页面，提示用户如何使用
@@ -30,295 +47,105 @@ int Mean(void);
 函数名称:   ReadScore
 功能描述:   录入每个学生的学号和成绩,并返回总人数
 参数:
-    num[]:学号
-    score[]:分数
-    n:总人数
-返回:总人数
-备注：弃用*/
-//int ReadScore(long num[], float score[], int n);
-/*
-函数名称:   ReadScore
-功能描述:   录入每个学生的学号和成绩,并返回总人数
-参数:
-    num[]:学号
-    name[]:姓名
-    score[]:分数
-    n：总人数
-返回:总人数
-备注：弃用*/
-int ReadScore(long num[], char name[][MAX_LEN], float score[], int n);
-/*
-函数名称:   ReadScore
-功能描述:   录入每个学生的学号和成绩,并返回总人数
-参数:
-    num[]:学号
-    name[]:姓名
-    score[][COURSE_NUM]:每门课分数
+    STU students:学生结构体数组
     n:实际总人数
     m:实际课程数
 返回:变化后总人数
 备注：*/
-int ReadScore(long num[], char name[][MAX_LEN], float score[][COURSE_NUM], int n, int m);
-/*
-函数名称:   AverSumofScore
-功能描述:   计算总分和平均值并输出
-参数:
-    score[]:分数
-    n:总人数
-返回:
-备注：弃用*/
-int AverSumofScore(float score[], int n);
+int ReadScore(STU students[], int n, int m);
 /*
 函数名称:   AverSumofEveryCourse
 功能描述:   计算每门课程的总分和平均分
 参数:
-    score[][COURSE_NUM]:分数
+    STU students:学生结构体数组
     n:总人数
     m:课程总数
 返回:
 备注：*/
-void AverSumofEveryCourse(float score[][COURSE_NUM], int n, int m);
+void AverSumofEveryCourse(STU students[], int n, int m);
 /*
 函数名称:   AverSumofEveryCourse
 功能描述:   计算每个学生的总分和平均分
 参数:
-    score[][COURSE_NUM]:分数
-    sum[]:学生的总分
-    aver[]:学生的平均分
+    STU students:学生结构体数组
     n:总人数
     m:课程总数
 返回:
 备注：*/
-void AverSumofEveryStudent(float score[][COURSE_NUM], float sum[], float aver[], int n, int m);
-/*
-函数名称:   DeSortbyScore
-功能描述:   按照成绩降序排列名次表
-参数:
-    num[]:学号
-    score[]:分数
-    n:总人数
-返回:
-备注：已弃用，被使用函数指针参数的SortbyScore替用*/
-//void DeSortbyScore(long num[], float score[], int n);
-/*
-函数名称:   SortbyScore
-功能描述:   按照成绩排列名次表,使用函数指针表达升降序
-参数:
-    num[]:学号
-    score[]:分数
-    n:总人数
-    *compare：比较函数指针
-返回:
-备注：老版已弃用*/
-//void SortbyScore(long num[], float score[], int n, int (*compare)(int a, int b));
-/*
-函数名称:   SortbyScore
-功能描述:   按照成绩排列名次表,使用函数指针表达升降序
-参数:
-    num[]:学号
-    name[]:姓名
-    score[]:分数
-    n:总人数
-    *compare：比较函数指针
-返回:
-备注：弃用*/
-//void SortbyScore(long num[], char name[][MAX_LEN], float score[], int n, int (*compare)(int a, int b));
+void AverSumofEveryStudent(STU students[], int n, int m);
 /*
 函数名称:   SortbyScore
 功能描述:   按照学生总分排列名次表,使用函数指针表达升降序
 参数:
-    num[]:学号
-    name[]:姓名
-    score[]:分数
-    sum[]:学生的总分
-    aver[]:学生的平均分
+    STU students:学生结构体数组
     n:总人数
     m:课程总数
     *compare：比较函数指针
 返回:
 备注：*/
-void SortbyScore(long num[], char name[][MAX_LEN], float score[][COURSE_NUM], float sum[], float aver[], int n, int m, int (*compare)(int a, int b));
-/*
-函数名称:   AsSortbyNum
-功能描述:   按照学号升序排列
-参数:
-    num[]:学号
-    score[]:分数
-    n:总人数
-返回:
-备注：已弃用，被使用函数指针参数的SortbyNum替用*/
-//void AsSortbyNum(long num[], float score[], int n);
+void SortbyScore(STU students[], int n, int m, int (*compare)(int a, int b));
 /*
 函数名称:   SortbyNum
 功能描述:   按照学号升序排列
 参数:
-    num[]:学号
-    score[]:分数
-    n:总人数
-    *compare：比较函数指针
-返回:
-备注:老版已弃用*/
-//void SortbyNum(long num[], float score[], int n, int(*compare)(int a, int b));
-/*
-函数名称:   SortbyNum
-功能描述:   按照学号升序排列
-参数:
-    num[]:学号
-    name[]:姓名
-    score[]:分数
-    n:总人数
-    *compare：比较函数指针
-返回:
-备注弃用*/
-//void SortbyNum(long num[], char name[][MAX_LEN], float score[], int n, int(*compare)(int a, int b));
-/*
-函数名称:   SortbyNum
-功能描述:   按照学号升序排列
-参数:
-    num[]:学号
-    name[]:姓名
-    score[]:分数
-    sum[]:学生的总分
-    aver[]:学生的平均分
+    STU students:学生结构体数组
     n:总人数
     m:课程总数
     *compare：比较函数指针
 返回:
 备注:*/
-void SortbyNum(long num[], char name[][MAX_LEN], float score[][COURSE_NUM], float sum[], float aver[], int n, int m, int (*compare)(int a, int b));
+void SortbyNum(STU students[], int n, int m, int (*compare)(int a, int b));
 /*
 函数名称:   SortbyName
 功能描述:   按照姓名字典顺序排列
 参数:
-    num[]:学号
-    name[]:姓名
-    score[]:分数
-    n:总人数
-返回:
-备注弃用*/
-//void SortbyName(long num[], char name[][MAX_LEN], float score[], int n);
-/*
-函数名称:   SortbyName
-功能描述:   按照姓名字典顺序排列
-参数:
-    num[]:学号
-    name[]:姓名
-    score[]:分数
-    sum[]:学生的总分
-    aver[]:学生的平均分
+    STU students:学生结构体数组
     n:总人数
     m:课程总数
 返回:
 备注*/
-void SortbyName(long num[], char name[][MAX_LEN], float score[][COURSE_NUM], float sum[], float aver[], int n, int m);
-/*
-函数名称:   SearchbyNum
-功能描述:   按学号查询学生排名及其考试成绩
-参数:
-    num[]:学号
-    name[]:姓名
-    score[]:分数
-    n:总人数
-返回:索引号
-备注：弃用*/
-//int SearchbyNum(long num[], char name[][MAX_LEN], float score[], int n);
+void SortbyName(STU students[], int n, int m);
 /*
 函数名称:   SearchbyNum
 功能描述:   按学号查询学生排名及其各科考试成绩；
 参数:
-    num[]:学号
-    name[]:姓名
-    score[]:分数
-    sum[]:学生的总分
-    aver[]:学生的平均分
+    STU students:学生结构体数组
     n:总人数
     m:课程总数
 返回:索引号
 备注：*/
-int SearchbyNum(long num[], char name[][MAX_LEN], float score[][COURSE_NUM], float sum[], float aver[], int n, int m);
-/*
-函数名称:   SearchbyName
-功能描述:   按学号查询学生排名及其考试成绩
-参数:
-    num[]:学号
-    name[]:姓名
-    score[]:分数
-    n:总人数
-返回:索引号
-备注：弃用*/
-//int SearchbyName(long num[], char name[][MAX_LEN], float score[], int n);
+int SearchbyNum(STU students[], int n, int m);
 /*
 函数名称:   SearchbyName
 功能描述:   按姓名查询学生排名及其各科考试成绩；
 参数:
-    num[]:学号
-    name[]:姓名
-    score[]:分数
-    sum[]:学生的总分
-    aver[]:学生的平均分
+    STU students:学生结构体数组
     n:总人数
     m:课程总数
 返回:索引号
 备注：*/
-int SearchbyName(long num[], char name[][MAX_LEN], float score[][COURSE_NUM], float sum[], float aver[], int n, int m);
-/*
-函数名称:   StatistAnalysis
-功能描述:   按优秀(90 - 100)、良好(80 - 89)、中等(70 - 79)、及格(60 - 69)、不及格（0 - 59)5个类别, 
-                 统计每个类别的人数以及所占的百分比;
-参数:
-    num[]:学号
-    score[]:分数
-    n:总人数
-返回:
-备注：*/
-//void StatistAnalysis( float score[], int n);
+int SearchbyName(STU students[], int n, int m);
 /*
 函数名称:   StatistAnalysis
 功能描述:   按优秀(90- 100)、良好(80- 89)、中等(70-79)、及格(60- 69)、不及格（0-59)5个类别,
                  对每门课程分别统计每个类别的人数以及所占的百分比;
 参数:
-    num[]:学号
-    score[][COURSE_NUM]:分数
+    STU students:学生结构体数组
     n:总人数
     m:总课程数
 返回:
 备注：*/
-void StatistAnalysis(float score[][COURSE_NUM], int n, int m);
-/*
-函数名称:   PrintScore
-功能描述:   打印每位学生的成绩
-参数:
-    num[]:学号
-    score[]:分数
-    n:总人数
-返回：
-备注：弃用*/
-//void PrintScore(long num[], float score[],int n);
+void StatistAnalysis(STU students[], int n, int m);
 /*
 函数名称:   PrintScore
 功能描述:   打印每位学生的学号，姓名和成绩
 参数:
-    num[]:学号
-    name[]:姓名
-    score[]:分数
-    n:总人数
-返回：
-备注：弃用*/
-//void PrintScore(long num[], char name[][MAX_LEN], float score[], int n);
-/*
-函数名称:   PrintScore
-功能描述:   打印每位学生的学号，姓名和成绩
-参数:
-    num[]:学号
-    name[][MAX_LEN]:姓名
-    score[][COURSE_NUM]:每门课
-    sum[]:学生的总分
-    aver[]:学生的平均分
+    STU students:学生结构体数组
     n:总人数
     m:课程总数
 返回：
 备注：*/
-void PrintScore(long num[], char name[][MAX_LEN], float score[][COURSE_NUM], float sum[], float aver[], int n, int m);
+void PrintScore(STU students[], int n, int m);
 /*
 函数名称:   FloatSwap
 功能描述:   交换两个浮点型变量
